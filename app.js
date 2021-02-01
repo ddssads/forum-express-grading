cb532778df79e8d6b4b7e34e74b80940b7289e89
ddssads <tw0937608360@gmail.com>
2021-02-01 23:35:41 +0800

    Add imgur api & dotenv

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const session = require('express-session')
 const flash = require('connect-flash')
 const passport = require('./config/passport')
 const methodOverride = require('method-override')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const db = require('./models')
 const app = express()
 const port = process.env.PORT || 3000
