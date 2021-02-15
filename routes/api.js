659dcdb92a8f98692bdcb69cd1d8a32f219765ca
ddssads <tw0937608360@gmail.com>
2021-02-15 20:32:34 +0800

    add api routes about get restaurant and categories

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,26 @@
 const express = require('express')
 const router = express.Router()
 const adminController = require('../controllers/adminController')
+const categoryController = require('../controllers/categoryController')
 const handleErrorAsync = require('../_helpers').handleErrorAsync
+
 
 
 router.get('/admin/restaurants', handleErrorAsync(async (req, res, next) => {
   const restaurants = await adminController.getRestaurants()
   res.json({ restaurants })
+}))
+
+//瀏覽單一餐廳資訊
+router.get('/admin/restaurants/:id', handleErrorAsync(async (req, res, next) => {
+  const restaurant = await adminController.getRestaurant(req.params.id)
+  res.json({ restaurant })
+}))
+
+//顯示分類頁面
+router.get('/admin/categories', handleErrorAsync(async (req, res, next) => {
+  const categories = await categoryController.getCategories()
+  return res.json({ categories })
 }))
 
 module.exports = router
